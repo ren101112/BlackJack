c83b129c5d40fc7a77c9d12594110c0e07fdfda4
renhirose <ren_hirose28@milton.edu>
2026-05-07 10:56:42 -0400

    tried to work on the array

--- src/basicGameApp.java
+++ src/basicGameApp.java
@@ -1,15 +1,28 @@
-public class basicGameApp {
+import javax.xml.namespace.QName;
 
+public class basicGameApp {
+//todo: SOMEWHAT STRAIGHTFORWARD: MAKE A CONSTRUCTOR THAT HAS 3 parameters
+    public card[] deck;
 
 
     public static void main(String[] args) {
 
+basicGameApp game =new basicGameApp();
 
     }
 
     public basicGameApp(){
+        System.out.println("welcome to blackjack");
         player me = new player();
         dealer d = new dealer();
+        deck =new card[52];
+
+        for (int x=0; x<13; x++){
+            deck[x]=new card("hearts",x,10);
+
+
+            deck[x].printInfo();
+        }
 
        compare();
        shuffle();
--- src/card.java
+++ src/card.java
@@ -4,8 +4,24 @@ public String suit;
 public String name;
 public int value;
 
+public card(String Psuit, int Pname, int Pvalue){
+    suit=Psuit;
+    //name=Pname;
+    value=Pvalue;
+    if (Pname==0){
+        name="ace";
+
+    }
 
 
+
+printInfo();
+}
+
+public void printInfo(){
+    System.out.println("this card is the "+name+" of "+suit+" worth "+value+" points");
+
+}
 
 
 
