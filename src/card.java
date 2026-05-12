2aa647eea9edca5b4341dee41165771d848f214a
renhirose <ren_hirose28@milton.edu>
2026-05-12 09:27:49 -0400

    ADDED all of the card types

--- src/card.java
+++ src/card.java
@@ -12,11 +12,50 @@ public card(String Psuit, int Pname, int Pvalue){
         name="ace";
 
     }
-
-
-
-printInfo();
+    if (Pname == 0) {
+        name = "ace";
+        value = 11;
+    } else if (Pname == 1) {
+        name = "two";
+        value = 2;
+    } else if (Pname == 2) {
+        name = "three";
+        value = 3;
+    } else if (Pname == 3) {
+        name = "four";
+        value = 4;
+    } else if (Pname == 4) {
+        name = "five";
+        value = 5;
+    } else if (Pname == 5) {
+        name = "six";
+        value = 6;
+    } else if (Pname == 6) {
+        name = "seven";
+        value = 7;
+    } else if (Pname == 7) {
+        name = "eight";
+        value = 8;
+    } else if (Pname == 8) {
+        name = "nine";
+        value = 9;
+    } else if (Pname == 9) {
+        name = "ten";
+        value = 10;
+    } else if (Pname == 10) {
+        name = "jack";
+        value = 10;
+    } else if (Pname == 11) {
+        name = "queen";
+        value = 10;
+    } else if (Pname == 12) {
+        name = "king";
+        value = 10;
+    }
 }
+
+
+
 
 public void printInfo(){
     System.out.println("this card is the "+name+" of "+suit+" worth "+value+" points");
